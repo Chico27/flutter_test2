8604a6fc5fc1414c8c36b4db3aeca9e2d786b76f
Chico27 <masamasa.987654321@gmail.com>
2024-03-02 21:28:18 +0900

    add.

---
+++ banana/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  final col = Column(
+
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    
+    children: const [
+    Text('レモン'),
+    Text('りんご'),
+    Text('ブドウ'),
+  ]);
+  final row = Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [col, col, col],
+  );
+  final a = MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: row,
+      ),
+    )
+  );
+  runApp(a);
+}
